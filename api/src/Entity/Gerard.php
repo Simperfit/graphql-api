f4f4c8457e1bb68fab80cdb33a8bb897d551b897
Amrouche Hamza <hamza@les-tilleuls.coop>
2018-04-12 16:38:56 +0200

    api-platform with graphql

--- api/src/Entity/Gerard.php
+++ api/src/Entity/Gerard.php
@@ -2,16 +2,21 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * This is a dummy entity. Remove it!
  *
+ * @ApiResource
+ *
  * @ORM\Entity
  */
-class Greeting
+class Gerard
 {
     /**
      * @var int The entity Id
@@ -29,6 +34,13 @@ class Greeting
      * @Assert\NotBlank
      */
     public $name = '';
+
+
+    public function __construct()
+    {
+        $this->greetings = new ArrayCollection();
+        $this->hellos = new ArrayCollection();
+    }
 
     public function getId(): int
     {
--- api/src/Entity/Hello.php
+++ api/src/Entity/Hello.php
@@ -9,9 +9,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * This is a dummy entity. Remove it!
  *
+ * @ApiResource
+ * 
  * @ORM\Entity
  */
-class Greeting
+class Hello
 {
     /**
      * @var int The entity Id
